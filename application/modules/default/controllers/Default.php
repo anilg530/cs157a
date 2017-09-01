0d0284061338ec7138583eba3878717e5b31d1bc
Trinh Nguyen <trinhnguyen@MacBook-Pro-2.local>
2017-09-01 11:15:57 -0700

    full environment set up

--- application/modules/default/controllers/Default.php
+++ application/modules/default/controllers/Default.php
@@ -1,7 +1,4 @@
-<?php
-
-if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Default extends CI_Controller {
 
