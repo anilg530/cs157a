95a1dd911cc47be4326171dd9a4669f30f2dc864
Trinh Nguyen <trinhnguyen@MacBook-Pro-2.local>
2017-09-01 11:06:11 -0700

    additional commit

---
+++ application/modules/default/views/default_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="#" />
+	<meta name="author" content="#" />
+	<link rel="icon" type="image/x-icon" href="#">
+
+	<title>Default Page</title>
+
+</head>
+<body>
+	<h4><?php echo "Hello there!"; ?></h4>
+</body>
+</html>
