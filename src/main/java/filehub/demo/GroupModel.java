e8251403af7e65d574d883ebd9a0d4192e358a11
Trinh Nguyen <>
2017-09-14 03:07:24 -0700

    basic DB queries

---
+++ src/main/java/filehub/demo/GroupModel.java
@@ -0,0 +1,117 @@
+package filehub.demo;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class GroupModel {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://p3plcpnl0569.prod.phx3.secureserver.net:3306/cs157a";
+    static final String USER = "cs157a_main";
+    static final String PASS = "cs157a_db";
+
+    public static ArrayList<Groups> getAllGroup() {
+        ArrayList<Groups> returnGroup = new ArrayList<>();
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(JDBC_DRIVER).newInstance();
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String myQuery;
+            myQuery = "SELECT * FROM groups";
+            ResultSet allGroup = stmt.executeQuery(myQuery);
+
+            if (allGroup != null) {
+                try {
+                    while (allGroup.next()) {
+                        int id = allGroup.getInt("id");
+                        String group_name = allGroup.getString("group_name");
+                        int group_owner = allGroup.getInt("group_owner");
+                        String group_password = allGroup.getString("group_password");
+                        String group_status = allGroup.getString("group_status");
+                        String created_on = allGroup.getString("created_on");
+                        System.out.println(created_on);
+                        returnGroup.add(new Groups(id, group_name, group_owner, group_password, group_status, created_on));
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            stmt.close();
+            conn.close();
+            allGroup.close();
+            return returnGroup;
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static void insertGroupTest() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName(JDBC_DRIVER).newInstance();
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+            String myQuery;
+            myQuery = "INSERT INTO groups (group_name, group_owner, group_password, group_status) " +
+                    "VALUES ('chicken', 3, 'frog', 'Active');";
+            stmt.executeUpdate(myQuery);
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public static void testSQLConnectionWorking() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            System.out.println("Connection Not OK");
+        } finally {
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException ignore) {
+            }
+        }
+    }
+}
---
+++ src/main/java/filehub/demo/Groups.java
@@ -0,0 +1,74 @@
+package filehub.demo;
+
+public class Groups {
+    private int id;
+    private String group_name;
+    private int group_owner;
+    private String group_password;
+    private String group_status;
+    private String created_on;
+
+
+    public Groups(int id, String group_name, int group_owner, String group_password, String group_status, String created_on) {
+        this.id = id;
+        this.group_name = group_name;
+        this.group_owner = group_owner;
+        this.group_password = group_password;
+        this.group_status = group_status;
+        this.created_on = created_on;
+    }
+
+    @Override
+    public String toString() {
+        return "Group[id:"+id+",group name:"+group_name+",group owner:"+group_owner+
+                ",group password:"+group_password+",group status:"+group_status+",created on:"+created_on+"]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public void setGroup_name(String group_name) {
+        this.group_name = group_name;
+    }
+
+    public int getGroup_owner() {
+        return group_owner;
+    }
+
+    public void setGroup_owner(int group_owner) {
+        this.group_owner = group_owner;
+    }
+
+    public String getGroup_password() {
+        return group_password;
+    }
+
+    public void setGroup_password(String group_password) {
+        this.group_password = group_password;
+    }
+
+    public String getGroup_status() {
+        return group_status;
+    }
+
+    public void setGroup_status(String group_status) {
+        this.group_status = group_status;
+    }
+
+    public String getCreated_on() {
+        return created_on;
+    }
+
+    public void setCreated_on(String created_on) {
+        this.created_on = created_on;
+    }
+}
