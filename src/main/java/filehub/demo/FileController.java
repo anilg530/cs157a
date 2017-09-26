bd03d8606f1c6764abbc384c1029e163c0fbe925
jennifernghinguyen <jennifernghi@yahoo.com>
2017-09-26 12:13:31 -0700

    ok

--- src/main/java/filehub/demo/FileController.java
+++ src/main/java/filehub/demo/FileController.java
@@ -2,6 +2,7 @@ package filehub.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
