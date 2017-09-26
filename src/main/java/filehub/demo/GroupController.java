9a216a7a3466c14f1f708bf9a7177eb44f0c7539
Trinh Nguyen <>
2017-09-26 16:33:29 -0700

    more file restoration

--- src/main/java/filehub/demo/GroupController.java
+++ src/main/java/filehub/demo/GroupController.java
@@ -3,33 +3,19 @@ package filehub.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
-
-import java.text.SimpleDateFormat;
->>>>>>> parent of 58b89ae... session, navigation
 import java.util.ArrayList;
 import java.util.UUID;
 
-@Scope("session")
 @Controller
 public class GroupController {
 
     @RequestMapping("group")
     public String main(Model model) {
-<<<<<<< HEAD
         System.out.println("hi there"+ UUID.randomUUID());
-=======
-        System.out.println("Timestamp: "+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime()));
->>>>>>> parent of 58b89ae... session, navigation
         model.addAttribute("page_name", "Group Page 01");
         return "group_page";
     }
 
-<<<<<<< HEAD
-=======
-    @RequestMapping("group/create_group")
->>>>>>> parent of 58b89ae... session, navigation
     public String createGroup(Model model) {
         model.addAttribute("page_name", "Create Group");
         return "group_page";
@@ -38,10 +24,6 @@ public class GroupController {
     @RequestMapping("group/add")
     public String createGroupTest(Model model) {
         model.addAttribute("page_name", "Create Group Test");
-<<<<<<< HEAD
-=======
-        GroupModel.insertGroupTest();
->>>>>>> parent of 58b89ae... session, navigation
         return "group_page";
     }
 
--- src/main/java/filehub/demo/LoginController.java
+++ src/main/java/filehub/demo/LoginController.java
@@ -1,26 +1,22 @@
 package filehub.demo;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
->>>>>>> parent of 58b89ae... session, navigation
 
-@Scope("session")
 @Controller
 public class LoginController {
 
     @RequestMapping(value={"/", "login"})
-<<<<<<< HEAD
-    public String login(Model model) {
-        System.out.println("hello");
-=======
     public String login(HttpServletRequest request, HttpSession session, Model model) {
         if (request.getMethod().equals("POST")) {
             String username = request.getParameter("username");
@@ -28,12 +24,24 @@ public class LoginController {
             System.out.println(username);
             System.out.println(password);
             model.addAttribute("username", username);
+            // the user_id should be retrieved from DB based on the correct
+            // username provided and set it below
             request.getSession().setAttribute("user_id",1);
             request.getSession().setAttribute("username",username);
         }
->>>>>>> parent of 58b89ae... session, navigation
         model.addAttribute("page_name", "FileHub Login Page");
         return "login_page";
+    }
+
+    @RequestMapping(value={"/logout"})
+    public void logout(HttpServletResponse response, HttpSession session) {
+        session.removeAttribute("user_id");
+        session.removeAttribute("username");
+        try {
+            response.sendRedirect("/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
