03e933e7222c7f90a8157485f824a3cbdd9bff3a
jennifernghinguyen <jennifernghi@yahoo.com>
2017-10-07 13:40:29 -0700

    add group

--- src/main/java/filehub/demo/GroupController.java
+++ src/main/java/filehub/demo/GroupController.java
@@ -3,6 +3,9 @@ package filehub.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -15,15 +18,24 @@ public class GroupController {
         model.addAttribute("page_name", "Group Page 01");
         return "group_page";
     }
-
+    @RequestMapping("group/create_group")
     public String createGroup(Model model) {
         model.addAttribute("page_name", "Create Group");
-        return "group_page";
+        return "group_create";
     }
 
-    @RequestMapping("group/add")
-    public String createGroupTest(Model model) {
-        model.addAttribute("page_name", "Create Group Test");
+    @RequestMapping("group/create_group/add")
+    public String createGroupTest(HttpServletRequest request, HttpSession session, Model model) {
+        model.addAttribute("page_name", "Create Group");
+        String groupname = request.getParameter("group_name");
+        String groupPassword = request.getParameter("group_password");
+        if(request.getMethod().equals("POST")){
+            int userID = (int) session.getAttribute("user_id");
+            System.out.println("group name: " + groupname);
+            System.out.println("group pass: " + groupPassword);
+            GroupModel.insertGroup(groupname, userID, groupPassword);
+
+        }
         return "group_page";
     }
 
